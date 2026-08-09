b0b498a6de5dc0ae4ff86e0887e45e34ee2732b5
devoutlierufo <devoutlierblog@gmail.com>
2026-08-08 22:01:06 -0400

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,266 @@
+"const playlist = [
+  {
+    ""title"": ""UFOs and Nuclear Weapons - A Fascinating Connection"",
+    ""videoId"": ""-DK2u8HlhDE"",
+    ""timestamp"": ""13:22"",
+    ""start"": 802
+  },
+  {
+    ""title"": ""Global Air Force UFO Encounters You've Probably Never Heard of"",
+    ""videoId"": ""6cVe-hdMTCE"",
+    ""timestamp"": ""12:41"",
+    ""start"": 761
+  },
+  {
+    ""title"": ""Incredible UFO Footage - FLYBY"",
+    ""videoId"": ""qD1KnudMCO0"",
+    ""timestamp"": ""15:34"",
+    ""start"": 934
+  },
+  {
+    ""title"": ""The Origin of the UFO Stigma"",
+    ""videoId"": ""xKArN8S9bnM"",
+    ""timestamp"": ""22:25"",
+    ""start"": 1345
+  },
+  {
+    ""title"": ""Incredible UFO Footage - METAPOD"",
+    ""videoId"": ""i6fitvV-aO0"",
+    ""timestamp"": ""17:07"",
+    ""start"": 1027
+  },
+  {
+    ""title"": ""SOL Foundation: Karl Nell - A Key Figure in UAP Disclosure"",
+    ""videoId"": ""kKbSIfc7N7Q"",
+    ""timestamp"": ""21:23"",
+    ""start"": 1283
+  },
+  {
+    ""title"": ""UFOs in the Private Sector - Battelle Memorial Institute"",
+    ""videoId"": ""tISTJRPOqFo"",
+    ""timestamp"": ""14:42"",
+    ""start"": 882
+  },
+  {
+    ""title"": ""UFOs in the Private Sector - Lockheed Martin"",
+    ""videoId"": ""tVdzvvbbHW4"",
+    ""timestamp"": ""28:34:00"",
+    ""start"": 102840
+  },
+  {
+    ""title"": ""FASTWALKERS – UFOs Outside Earth"",
+    ""videoId"": ""Tubx-CaAaOs"",
+    ""timestamp"": ""20:24"",
+    ""start"": 1224
+  },
+  {
+    ""title"": ""The 1953 Kingman, Arizona UFO Crash"",
+    ""videoId"": ""41V4Pf_8oo4"",
+    ""timestamp"": ""18:44"",
+    ""start"": 1124
+  },
+  {
+    ""title"": ""The Physics of UFOs– Dr. Kevin Knuth"",
+    ""videoId"": ""inyw4Vfu7Z0"",
+    ""timestamp"": ""19:52"",
+    ""start"": 1192
+  },
+  {
+    ""title"": ""The 1933 Magenta, Italy UFO Crash"",
+    ""videoId"": ""FGiwgyLY7Aw"",
+    ""timestamp"": ""23:04"",
+    ""start"": 1384
+  },
+  {
+    ""title"": ""Michael Herrera: UFO Whistleblower (ft. Joeyisnotmyname)"",
+    ""videoId"": ""6DyTfIV87Ck"",
+    ""timestamp"": ""52:37:00"",
+    ""start"": 189420
+  },
+  {
+    ""title"": ""Dr. Robert Sarbacher & the US Government's Secret UFO Crash Retrieval Group"",
+    ""videoId"": ""R7DyJRf14nU"",
+    ""timestamp"": ""19:35"",
+    ""start"": 1175
+  },
+  {
+    ""title"": ""USO - Unidentified Submerged Objects"",
+    ""videoId"": ""U-jrx_giINA"",
+    ""timestamp"": ""24:42:00"",
+    ""start"": 88920
+  },
+  {
+    ""title"": ""USO Case Book: Unidentified Submerged Objects Throughout History"",
+    ""videoId"": ""Yn-83KbgRN8"",
+    ""timestamp"": ""24:48:00"",
+    ""start"": 89280
+  },
+  {
+    ""title"": ""UFO Whistleblowers [Vol.1]"",
+    ""videoId"": ""EKEZ2BClIb0"",
+    ""timestamp"": ""26:15:00"",
+    ""start"": 94500
+  },
+  {
+    ""title"": ""The 1950s Del Rio, Texas UFO Crashes"",
+    ""videoId"": ""8S9qdRWSnD8"",
+    ""timestamp"": ""27:09:00"",
+    ""start"": 97740
+  },
+  {
+    ""title"": ""UFO Whistleblowers [Vol.2]"",
+    ""videoId"": ""9Xk4X41yl2M"",
+    ""timestamp"": ""26:49:00"",
+    ""start"": 96540
+  },
+  {
+    ""title"": ""US Special Forces Confession - I Recovered Crashed UFOs: Fact or Fiction?"",
+    ""videoId"": ""DcvuglS7ps4"",
+    ""timestamp"": ""39:38:00"",
+    ""start"": 142680
+  },
+  {
+    ""title"": ""Off-World Technologies Division – UAP Technology Reverse Engineering"",
+    ""videoId"": ""QJ215I85d5M"",
+    ""timestamp"": ""49:05:00"",
+    ""start"": 176700
+  },
+  {
+    ""title"": ""The 1974 Coyame, Mexico UFO Crash"",
+    ""videoId"": ""bL3tMByq_WM"",
+    ""timestamp"": ""30:29:00"",
+    ""start"": 109740
+  },
+  {
+    ""title"": ""The Majestic-12 Documents [With Ryan S. Wood]"",
+    ""videoId"": ""vzB87RJkQVU"",
+    ""timestamp"": ""1:30:29"",
+    ""start"": 5429
+  },
+  {
+    ""title"": ""The 1965 Kecksburg, Pennsylvania UFO Crash"",
+    ""videoId"": ""rgBTMzFd-hg"",
+    ""timestamp"": ""1:06:32"",
+    ""start"": 3992
+  },
+  {
+    ""title"": ""UAP Reverse Engineering at Edwards Air Force Base [Redacted List Vol.2]"",
+    ""videoId"": ""U_LSMLGBDNg"",
+    ""timestamp"": ""41:17:00"",
+    ""start"": 148620
+  },
+  {
+    ""title"": ""Alien Reproduction Vehicle - the Testimony of Mark McCandlish"",
+    ""videoId"": ""wF07QMm6joE"",
+    ""timestamp"": ""1:00:00"",
+    ""start"": 3600
+  },
+  {
+    ""title"": ""Alien Reproduction Vehicle - TR-3B and the Flying Triangles"",
+    ""videoId"": ""B7JP0uX0GwY"",
+    ""timestamp"": ""1:06:40"",
+    ""start"": 4000
+  },
+  {
+    ""title"": ""Philip J. Corso - US Army UFO Technology Research & Development"",
+    ""videoId"": ""_sv0Otxtcn4"",
+    ""timestamp"": ""1:22:21"",
+    ""start"": 4941
+  },
+  {
+    ""title"": ""UFO Legacy Programs - Northrop Grumman"",
+    ""videoId"": ""E-GleS2aGj0"",
+    ""timestamp"": ""55:45:00"",
+    ""start"": 200700
+  },
+  {
+    ""title"": ""Dugway Proving Ground - UFO Legacy Programs"",
+    ""videoId"": ""rePrVmR6a68"",
+    ""timestamp"": ""1:04:25"",
+    ""start"": 3865
+  },
+  {
+    ""title"": ""Deep Underground Military Bases (D.U.M.Bs.) - UFO Legacy Programs"",
+    ""videoId"": ""7Ats0lIy3Lo"",
+    ""timestamp"": ""1:27:00"",
+    ""start"": 5220
+  },
+  {
+    ""title"": ""US Navy UFO Crash Retrieval & Reverse Engineering Programs"",
+    ""videoId"": ""H9GSqOEvoBE"",
+    ""timestamp"": ""1:36:32"",
+    ""start"": 5792
+  },
+  {
+    ""title"": ""1997 Peru UFO Crash Retrieval - the Story of Jonathan Weygandt"",
+    ""videoId"": ""7Jc2G5aEH0A"",
+    ""timestamp"": ""1:43:49"",
+    ""start"": 6229
+  },
+  {
+    ""title"": ""UFO Legacy Programs - Science Applications International Corporation (SAIC)"",
+    ""videoId"": ""9p99lTsC7wQ?t=7247"",
+    ""timestamp"": ""2:00:48"",
+    ""start"": 7248
+  },
+  {
+    ""title"": ""The 1948 Aztec, New Mexico UFO Crash Retrieval"",
+    ""videoId"": ""QJxbyu-9Tj0"",
+    ""timestamp"": ""2:12:39"",
+    ""start"": 7959
+  },
+  {
+    ""title"": ""1997 ALIEN REPRODUCTION VEHICLE ENCOUNTER AT 29 PALMS, CA - THE TESTIMONY OF SGT RODERICK CASTLE"",
+    ""videoId"": ""ShoUyC1aip0"",
+    ""timestamp"": ""1:44:19"",
+    ""start"": 6259
+  },
+  {
+    ""title"": ""Lockheed Martin - UFO Reverse Engineering, Material Exploitation, & Legacy Programs"",
+    ""videoId"": ""X6JfbfmvgMo"",
+    ""timestamp"": ""2:40:48"",
+    ""start"": 9648
+  },
+  {
+    ""title"": ""Sandia National Laboratories - UFO Reverse Engineering, Material Exploitation, & Legacy Programs"",
+    ""videoId"": ""mHkPHd_eOXI"",
+    ""timestamp"": ""2:34:25"",
+    ""start"": 9265
+  },
+  {
+    ""title"": ""National Reconnaissance Office - UFO Crash Retrievals, Surveillance, and Legacy Program Gatekeepers"",
+    ""videoId"": ""GM1Qx9vuo2c"",
+    ""timestamp"": ""2:51:28"",
+    ""start"": 10288
+  },
+  {
+    ""title"": ""The Hidden Wing - US Air Force UFO Reverse Engineering Programs"",
+    ""videoId"": ""-IXSZe4xVv4"",
+    ""timestamp"": ""3:16:08"",
+    ""start"": 11768
+  },
+  {
+    ""title"": ""Northrop Grumman & TRW - UFO Reverse Engineering, Material Exploitation, & Legacy Programs [Vol.2]"",
+    ""videoId"": ""0kLSp0lH-JQ"",
+    ""timestamp"": ""3:07:03"",
+    ""start"": 11223
+  },
+  {
+    ""title"": ""The Manhattan Project 2.0 - the Secrecy of UFO Crash Retrieval Programs [VOL.1]"",
+    ""videoId"": ""jR-h5p2bd-A"",
+    ""timestamp"": ""3:05:05"",
+    ""start"": 11105
+  },
+  {
+    ""title"": ""Special Access Required  - the Secrecy of UFO Crash Retrieval Programs [VOL.2]"",
+    ""videoId"": ""6MsmPrpQqa8"",
+    ""timestamp"": ""2:56:44"",
+    ""start"": 10604
+  },
+  {
+    ""title"": ""The MITRE Corporation - UFO Reverse Engineering, USO Tracking, & Legacy Programs"",
+    ""videoId"": ""107BtDYhTkA"",
+    ""timestamp"": ""1:57:26"",
+    ""start"": 7046
+  }
+];"
